23d4cabe9f40ecf016b1b8fdf24e6fc27fb0071e
Felipe Quintero <ffquint@gmail.com>
2020-10-23 12:57:44 -0400

    refactored code for task 5 so that only the feedback is updated without having to include all the keys/values that do not need to be changed or updated

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ Reyna's feedback is missing! Use what you know to do the following:
   2. log the reviews array to the console to check your work
 */
 
-reviews[7] = ({name: "Reyna", rating: 3.5, feedback: "this place is chill with really cool people, great for getting work done on weekdays"});
+reviews[7].feedback = ("this place is chill with really cool people, great for getting work done on weekdays");
 
 console.log(reviews);
 
